e4dedb91042d007d512ec9b383f322d26b8040b5
Brian Naqica <bnaqica@yahoo.com>
2019-11-26 20:11:24 -0700

    Initial Skeleton commit

---
+++ src/main/java/com/bnaqica/schoolorchestrator/SchoolOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schoolorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnaqica/schoolorchestrator/SchoolOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schoolorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
